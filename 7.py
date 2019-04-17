db0a95308e09453e27d81f6777df7499ec937a4c
Jefferson Chaves <jeffersonchaves@users.noreply.github.com>
2019-04-17 19:53:25 -0300

    Mudando a posição dos prints

--- 7.py
+++ 7.py
@@ -1,7 +1,6 @@
 temperatura_celsius = 27
 
-print("A temperatura em celsius é: ", temperatura_celsius)
-
 f = 9 * temperatura_celsius / 5 + 32
 
+print("A temperatura em celsius é: ", temperatura_celsius)
 print("A temperatura em fahrenheit: ", f)
